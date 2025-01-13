4edf3e075dc561ee260a87d72a5a00a3b34e47bf
terencengdev <terencejiahao@gmail.com>
2025-01-14 00:08:43 +0800

    remove console.log

--- backend/cloudinaryConfig.js
+++ backend/cloudinaryConfig.js
@@ -1,8 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
-console.log(process.env); // remove this after you've confirmed it is working
-
 // Cloudinary Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
