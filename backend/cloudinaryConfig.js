2c1adee92dd352b42685e1cd0d14640d830d0172
terencengdev <terencejiahao@gmail.com>
2025-01-13 23:06:06 +0800

    reconfig

--- backend/cloudinaryConfig.js
+++ backend/cloudinaryConfig.js
@@ -1,6 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
+console.log(process.env); // remove this after you've confirmed it is working
+
 // Cloudinary Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
