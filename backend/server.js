3462fe6ac010c49ce0255a9af8cacc4d0915b935
terencengdev <terencejiahao@gmail.com>
2025-01-13 13:56:30 +0800

    update vercel config

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,14 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// Handle requests by serving index.html for all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
